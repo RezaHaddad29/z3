2a9d3b2404ff8af4fda2303037e3bd12605136c7
Reza <rezahaddad29@gmail.com>
2021-08-26 16:24:12 +0430

    triangle puzzle

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 from z3 import *
 
 
-x = BitVec('x', 8)
-
-y = BitVec('y', 8)
-h = BitVec('h',8)
-slow = x * 2
-faster = x << h
+x1 = Int('x1')
+x2 = Int('x2')
+x3 = Int('x3')
+x4 = Int('x4')
+x5 = Int('x5')
+x6 = Int('x6')
+x7 = Int('x7')
+x8 = Int('x8')
+x9 = Int('x9')
 
 s = Solver()
-s.add(ForAll([x], slow == faster))
+
+for i in range(1,10):
+    s.add(eval("x{0} >0".format(i)))
+    s.add(eval("x{0} <10".format(i)))
+    for j in range(i,10):
+        if i!=j:
+            r = "x{0} != x{1}".format(i,j)
+            s.add(eval(r))
+    
+s.add(x1+x2+x3+x4 ==17)
+s.add(x4+x5+x6+x7 ==17)
+s.add(x7+x8+x9+x1 ==17)
 s.check()
 print(s.model())
